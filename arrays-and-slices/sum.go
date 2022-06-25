7806621a67abe6e23c0d31a8239ccf70903f17e2
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2022-06-25 17:33:30 +0200

    feat: :zap: Test da slices

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -27,8 +27,12 @@ func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 
 	for _, numbers := range numbersToSum {
-		tail := numbers[1:]
-		sums = append(sums, Sum(tail))
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 	return sums
 }
