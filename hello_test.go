d498382fb431caf111a376bb9c0420c0826675e4
AleksBeliun <aleksandr@novoresume.com>
2022-06-25 14:16:57 +0200

    test: :white_check_mark: Tinker with test

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Aleks")
-	want := "Hello, Aleks"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Aleks")
+		want := "Hello, Aleks"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
