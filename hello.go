2a96e13d235a9d8e389fd898961469943816e3a9
AleksBeliun <aleksandr@novoresume.com>
2022-06-25 14:20:58 +0200

    refactor: :recycle: Refactor Hello function

--- hello.go
+++ hello.go
@@ -13,13 +13,19 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+	return
 }
 
 func main() {
