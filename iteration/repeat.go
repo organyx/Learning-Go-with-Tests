09c149f62012e0a197250d29e6a94f32512ffc36
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2022-06-25 16:52:37 +0200

    feat: Trying out iterations

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,19 @@
 package iteration
 
+const repeatCount = 5
+
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
+
+func RepeatWithCount(character string, count int) string {
+	var repeated string
+	for i := 0; i < count; i++ {
+		repeated += character
 	}
 	return repeated
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -10,3 +10,18 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
+
+func TestRepeatWithCount(t *testing.T) {
+	repeated := RepeatWithCount("b", 3)
+	expected := "bbb"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
