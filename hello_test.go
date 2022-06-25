8ce77c518a13414eb5887867871ad202743cb001
AleksBeliun <aleksandr@novoresume.com>
2022-06-25 14:13:54 +0200

    test: :test_tube: Add first failing test

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Aleks")
+	want := "Hello, Aleks"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
