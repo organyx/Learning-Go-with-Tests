45f11d6ad11553ae65c3b2c1f87dfce891ae3d38
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2022-06-25 17:08:50 +0200

    refactor: :zap: Use range

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -3,8 +3,12 @@ package main
 func Sum(numbers [5]int) int {
 	sum := 0
 
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	// for i := 0; i < 5; i++ {
+	// 	sum += numbers[i]
+	// }
+
+	for _, number := range numbers {
+		sum += number
 	}
 
 	return sum
