4fdfe456d4d0640323ef84ef243588df9cd48edd
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2022-06-25 17:23:51 +0200

    feat: :sparkles: Write minimal code

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -13,3 +13,13 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
+}
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -26,4 +29,14 @@ func TestSum(t *testing.T) {
 	// 	}
 	// })
 
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{3, 9})
+	want := []int{3, 12}
+	// want := "Foo"
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
