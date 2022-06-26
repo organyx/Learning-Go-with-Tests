eb96e154cc3b023c6e1749e53f3044d043cc01f6
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2022-06-26 17:28:51 +0200

    feat: Refactor and add Triangle

--- structs-methods-and-interfaces/perimeter.go
+++ structs-methods-and-interfaces/perimeter.go
@@ -23,6 +23,15 @@ type Shape interface {
 	Area() float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
--- structs-methods-and-interfaces/perimeter_test.go
+++ structs-methods-and-interfaces/perimeter_test.go
@@ -33,21 +33,38 @@ func TestArea(t *testing.T) {
 	// 	}
 	// })
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("%#v got %g want %g", shape, got, want)
+	// checkArea := func(t testing.TB, shape Shape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		t.Errorf("%#v got %g want %g", shape, got, want)
+	// 	}
+	// }
+
+	// t.Run("rectangles", func(t *testing.T) {
+	// 	rectangle := Rectangle{10, 12}
+	// 	checkArea(t, rectangle, 120.0)
+	// })
+
+	// t.Run("circles", func(t *testing.T) {
+	// 	circle := Circle{10}
+	// 	checkArea(t, circle, 314.1592653589793)
+	// })
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10, 12}, 120.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
 		}
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10, 12}
-		checkArea(t, rectangle, 120.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
 }
