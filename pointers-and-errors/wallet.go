5b63dbe18e9c2baec5f0780ca7888f2e972b0ad5
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2022-06-27 21:22:59 +0200

    feat: :zap: Add Bitcoin as type

--- pointers-and-errors/wallet.go
+++ pointers-and-errors/wallet.go
@@ -2,15 +2,17 @@ package pointersanderrors
 
 import "fmt"
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
