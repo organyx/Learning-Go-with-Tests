0f54a73a45ebdd525b3e536749c91a40e7b3a274
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2022-06-27 21:43:51 +0200

    refactor: :zap: Refactor assertError function

--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -16,11 +16,15 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
-	assertError := func(t testing.TB, err error) {
+	assertError := func(t testing.TB, got error, want string) {
 		t.Helper()
 
-		if err == nil {
-			t.Error("expected an error but didn't get one")
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
@@ -46,7 +50,7 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
-		assertError(t, err)
+		assertError(t, err, "cannot withdraw, insufficient funds")
 		assertBalance(t, wallet, startingBalance)
 
 	})
