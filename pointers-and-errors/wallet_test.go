5032fed64e64c830a825b5bd932c339703f402ee
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2022-06-27 21:34:29 +0200

    refactor: :white_check_mark: Refactor tests

--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -1,25 +1,27 @@
 package pointersanderrors
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-
-		fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 
 	})
 
@@ -28,12 +30,6 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(Bitcoin(10))
 
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 }
