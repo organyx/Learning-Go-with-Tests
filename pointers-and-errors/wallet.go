266fa2997e1a594ad78a227ee90eb176b0caffeb
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2022-06-27 21:25:34 +0200

    refactor: :zap: Add string format for Bitcoin

--- pointers-and-errors/wallet.go
+++ pointers-and-errors/wallet.go
@@ -4,6 +4,14 @@ import "fmt"
 
 type Bitcoin int
 
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
 	balance Bitcoin
 }
