604bf3b845b784d4c2e355a75fe410ecdfe95099
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2022-06-29 19:45:02 +0200

    refactor: :recycle: Move error to separate variable

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -8,11 +8,13 @@ type Dictionary map[string]string
 // 	return dictionary[word]
 // }
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 
 	return definition, nil
