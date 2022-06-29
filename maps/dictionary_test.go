fb3b02250b65786515a05a74cf26a8a26ebb43c6
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2022-06-29 19:47:57 +0200

    test: :white_check_mark: Add test for Add function

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -34,3 +34,19 @@ func assertError(t testing.TB, got, want error) {
 		t.Errorf("got error %q want %q", got, want)
 	}
 }
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
