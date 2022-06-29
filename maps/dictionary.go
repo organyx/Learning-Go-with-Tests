ac7678d827db8f742899cfb89e90e89b3677a5cd
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2022-06-29 19:36:41 +0200

    feat: :zap: Add Dictionary type

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,11 @@
 package maps
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+type Dictionary map[string]string
+
+// func Search(dictionary map[string]string, word string) string {
+// 	return dictionary[word]
+// }
+
+func (d Dictionary) Search(word string) string {
+	return d[word]
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,12 +3,18 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
+	got := dictionary.Search("test")
 	want := "this is just a test"
 
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
 }
