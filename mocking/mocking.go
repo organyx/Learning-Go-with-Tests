ba24306df672a5bcf8e610f835632bfb4a9d6a5a
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2024-07-30 16:17:22 +0200

    Configurable Sleeper

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -15,6 +15,7 @@ const finalWord = "Go!"
 const countdownStart = 3
 
 func Countdown(out io.Writer, sleeper Sleeper) {
+
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
@@ -24,13 +25,16 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 
 }
 
-type DefaultSleeper struct{}
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
